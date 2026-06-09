c9a825c365089bb907ed1193259b8f36614fafec
katanaslay <231722259+denyssemenchuk-source@users.noreply.github.com>
2026-06-09 11:49:54 +0000

    feat: 13 Lab done

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Blog\Admin\CategoryController;
+use App\Http\Controllers\DiggingDeeperController; // Додано імпорт контролера
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,13 +9,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// --- ГРУПА 1: Адмінка блогу ---
 $groupData = [
     'namespace' => 'App\Http\Controllers\Api\Blog\Admin',
     'prefix' => 'admin/blog',
 ];
 
 Route::group($groupData, function () {
-
     // BlogCategory
     $methods = ['index', 'store', 'update', 'show'];
     Route::apiResource('categories', CategoryController::class)
@@ -25,5 +26,15 @@ Route::group($groupData, function () {
     Route::apiResource('posts', 'PostController')
         ->except(['show'])
         ->names('blog.admin.posts');
+});
+
+// --- ГРУПА 2: Digging Deeper (ОКРЕМО ВІД АДМІНКИ) ---
+Route::group(['prefix' => 'digging_deeper'], function () {
+
+    Route::get('process-video', [DiggingDeeperController::class, 'processVideo'])
+        ->name('digging_deeper.processVideo');
+
+    Route::get('prepare-catalog', [DiggingDeeperController::class, 'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
 
 });
